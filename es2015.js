3f9e9f2bb9083d525ca84ef8f1bf2234a3449252
hannatrap <107367091+hannatrap@users.noreply.github.com>
2022-06-19 14:50:37 -0500

    Add files via upload

---
+++ es2015.js
@@ -0,0 +1,7 @@
+const PI = 3.14
+
+//
+//Difference between var and let: var is function scoped and can be reassigned and redeclared and let is block scoped and can only be reassigned
+//difference between var and const: var is function scoped and const is block scoped, also const cannot be reassigned or redeclared like var
+//Difference between let and const: let can be re-assigned value and const cannoot
+//Hoisting: hoisting is the method of the declarations being run first for var and not for let and const - function delcarations can also be hosted an run before they are defined.
